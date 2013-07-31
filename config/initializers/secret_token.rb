b485288659077a228097f8bf79db7c1d4f7ae872
Daisuke Yamashita <dddaisuke@gmail.com>
2013-08-01 07:11:47 +0900

    rails new trello-development-dashboard

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrelloDevelopmentDashboard::Application.config.secret_key_base = 'befb8d61a724dce6f5a1af03958bdc23ddb641ae0b3f4c06d158665f267be0e36a6946cc2e7c849cf21b63cdbb2c203de023bf97bcd80f1b5c423d119fc2746c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TrelloDevelopmentDashboard::Application.config.session_store :cookie_store, key: '_trello-development-dashboard_session'
